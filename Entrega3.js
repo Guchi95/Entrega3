7c75403fca18e1217788576ea3c71c3615a5b906
LAPTOP-D3C288M3\Usuario <gvirsauskas@gmail.com>
2021-12-11 19:01:49 -0300

    init del proyecto npm y creacion del servidor

---
+++ Entrega3.js
@@ -0,0 +1,9 @@
+const express = require ('express');
+
+const app = express();
+
+const PORT = 8080
+
+const server = app.listen(PORT, () => {
+   console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+})
